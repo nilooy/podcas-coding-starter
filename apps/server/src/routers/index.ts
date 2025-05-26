17d992e9720ab9723348490b94a96ffbb01b2ca5
nilooy <nilooy.dev@gmail.com>
2025-05-26 13:00:28 +0200

    :zap: AI

--- apps/server/src/routers/index.ts
+++ apps/server/src/routers/index.ts
@@ -1,11 +1,11 @@
-import {
-  publicProcedure,
-  router,
-} from "../lib/trpc";
+import { publicProcedure, router } from "../lib/trpc";
 
 export const appRouter = router({
   healthCheck: publicProcedure.query(() => {
     return "OK";
+  }),
+  getTextString: publicProcedure.query(() => {
+    return "Life in Ocean";
   }),
 });
 export type AppRouter = typeof appRouter;
